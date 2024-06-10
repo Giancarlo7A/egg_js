81eef12bfbfa7aa5666498d490e6cc9a79cd3936
Giancarlo <giancarlo.arevalo7@gmail.com>
2024-06-10 00:04:36 -0500

    Class1

---
+++ example/scripts/index.js
@@ -0,0 +1,16 @@
+/* seleccionar una etiqueta del HTML */
+const selector = document.getElementById("mensaje");
+
+/* escribir dentro de esa etiqueta con JS */
+selector.innerHTML = "CONEXIÓN EXITOSA ENTRE HTML Y JS";
+
+prompt("Preguntar algo");
+console.log("Imprimir algo");
+
+const entrada = prompt("¿Qué producto desea comprar?");
+console.log(entrada);
+alert(entrada);
+
+const nombre = prompt("cual es tu nombre")
+const mensaje = "Bienvenido " + nombre;
+alert(mensaje)
